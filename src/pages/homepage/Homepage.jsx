1ba4f131d501021780a7f6c1cad2acbe9f90a607
Arijit_Banerjee <arijitbanerjee873@gmail.com>
2025-06-09 14:14:43 +0530

    test

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import HeroSection from "./components/heroSection/HeroSection";
+import Logo from "@/components/Logo";
 
 const Homepage = () => {
   return (
     <div>
+      <Logo />
       <HeroSection />
     </div>
   );
